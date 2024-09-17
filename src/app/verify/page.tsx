fce6f80587c356bdbd7af7ec92fe2a4b22c2c48e
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-09-17 23:44:06 +0500

    update on authorization

---
+++ src/app/verify/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+function Verify() {
+    return ( <>
+    Verify your account...
+    <br />
+    <button>Resend</button>
+    </> );
+}
+
+export default Verify;
---
+++ src/firebase/firebaseauth.ts
@@ -0,0 +1,31 @@
+import { getAuth, signInWithEmailAndPassword,  createUserWithEmailAndPassword  } from "firebase/auth";
+import { app } from "./firebaseconfig";
+
+const auth = getAuth(app);
+
+export function signInUser(email:string,password:string ) {
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  });
+}
+
+
+export function signUpUser(email:string,password:string ) {
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  });
+}
